e4cf595cc579e2b2167d52e3e598d2719e13d436
Glitch (slack-slash-command-and-dialogs-blueprint) <none>
2017-11-12 22:02:05 +0000

    🏵🍁 Updated with Glitch

---
+++ src/index.js
@@ -0,0 +1,107 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+const qs = require('querystring');
+const ticket = require('./ticket');
+const debug = require('debug')('slash-command-template:index');
+
+const app = express();
+
+/*
+ * Parse application/x-www-form-urlencoded && application/json
+ */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('<h2>The Slash Command and Dialog app is running</h2> <p>Follow the' +
+  ' instructions in the README to configure the Slack App and your environment variables.</p>');
+});
+
+/*
+ * Endpoint to receive /helpdesk slash command from Slack.
+ * Checks verification token and opens a dialog to capture more info.
+ */
+app.post('/commands', (req, res) => {
+  // extract the verification token, slash command text,
+  // and trigger ID from payload
+  const { token, text, trigger_id } = req.body;
+
+  // check that the verification token matches expected value
+  if (token === process.env.SLACK_VERIFICATION_TOKEN) {
+    // create the dialog payload - includes the dialog structure, Slack API token,
+    // and trigger ID
+    const dialog = {
+      token: process.env.SLACK_ACCESS_TOKEN,
+      trigger_id,
+      dialog: JSON.stringify({
+        title: 'Hi! Let\'s do a checkin.',
+        callback_id: 'submit-feedback',
+        submit_label: 'Submit',
+        elements: [
+          {
+            label: 'Fellow?',
+            type: 'text',
+            name: 'fellow',
+            value: text,
+            hint: 'Don\'t forget the @',
+          },
+          {
+            label: 'What is your work email?',
+            type: 'text',
+            value: text,
+            name: 'email',
+          },
+          {
+            label: 'What is your company?',
+            type: 'text',
+            value: text,
+            name: 'company',
+          },
+        ],
+      }),
+    };
+
+    // open the dialog by calling dialogs.open method and sending the payload
+    axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
+      .then((result) => {
+        debug('dialog.open: %o', result.data);
+        res.send('');
+      }).catch((err) => {
+        debug('dialog.open call failed: %o', err);
+        res.sendStatus(500);
+      });
+  } else {
+    debug('Verification token mismatch');
+    res.sendStatus(500);
+  }
+});
+
+/*
+ * Endpoint to receive the dialog submission. Checks the verification token
+ * and creates a Helpdesk ticket
+ */
+app.post('/interactive-component', (req, res) => {
+  const body = JSON.parse(req.body.payload);
+
+  // check that the verification token matches expected value
+  if (body.token === process.env.SLACK_VERIFICATION_TOKEN) {
+    debug(`Form submission received: ${body.submission.trigger_id}`);
+
+    // immediately respond with a empty 200 response to let
+    // Slack know the command was received
+    res.send('');
+
+    // create Helpdesk ticket
+    ticket.create(body.user.id, body.submission);
+  } else {
+    debug('Token mismatch');
+    res.sendStatus(500);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}!`);
+});
---
+++ src/ticket.js
@@ -0,0 +1,369 @@
+const axios = require('axios');
+const debug = require('debug')('slash-command-template:ticket');
+const qs = require('querystring');
+const users = require('./users');
+
+/*
+ *  Send ticket creation confirmation via
+ *  chat.postMessage to the user who created it
+ */
+const sendConfirmation = (ticket) => {
+  axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+    token: process.env.SLACK_ACCESS_TOKEN,
+    channel: ticket.userId,
+    text: `Submitting Feedback for ${ticket.fellow}`,
+    attachments: JSON.stringify([
+      {
+      text: ticket.text,
+      fallback: 'Pre-filled because you have actions in your attachment.',
+      color: '#db1017',
+      text: 'The ability to deliver work output that meets or exceeds expected standards of functionality, performance, reliability and maintainability.',
+      mrkdwn_in: [
+        'text',
+        'pretext',
+        'fields'
+      ],
+      callback_id: 'Pre-filled because you have actions in your attachment.',
+      attachment_type: 'default',
+      title: 'Work Quality',
+      actions: [
+        
+        {
+          name: 'Work Quality',
+          text: 'Work Quality',
+          type: 'select',
+          value: 'quality',
+          data_source: 'static',
+          options: [
+            {
+              text: '+2  Very Satisfied :green_heart: :green_heart: ',
+              value: '+2  Very Satisfied :green_heart: :green_heart: '
+            },
+            {
+              text: '+1  Satisfied :green_heart: ',
+              value: '+1  Satisfied :green_heart: '
+            },
+            {
+              text: '0 Neutral :yellow_heart: ',
+              value: '0 Neutral :yellow_heart: '
+            },
+            {
+              text: '-1  Unsatisfied :broken_heart: ',
+              value: '-1  Unsatisfied :broken_heart: '
+            },
+            {
+              text: '-2 Very Unsatisfied :broken_heart: :broken_heart:',
+              value: '-2 Very Unsatisfied :broken_heart: :broken_heart:'
+            }
+          ]
+        }
+      ]
+    },
+       {
+      text: ticket.text,
+      fallback: 'Pre-filled because you have actions in your attachment.',
+      color: '#ff8000',
+      text: 'The ability to consistently deliver work output of sufficient quality at or above the expected pace.',
+      mrkdwn_in: [
+        'text',
+        'pretext',
+        'fields'
+      ],
+      callback_id: 'Pre-filled because you have actions in your attachment.',
+      attachment_type: 'default',
+      title: 'Work Quantity',
+      actions: [
+        {
+          name: 'Work Quantity',
+          text: 'Work Quantity',
+          type: 'select',
+          value: 'quantity',
+          data_source: 'static',
+          options: [
+            {
+              text: '+2  Very Satisfied :green_heart: :green_heart: ',
+              value: '+2  Very Satisfied :green_heart: :green_heart: '
+            },
+            {
+              text: '+1  Satisfied :green_heart: ',
+              value: '+1  Satisfied :green_heart: '
+            },
+            {
+              text: '0 Neutral :yellow_heart: ',
+              value: '0 Neutral :yellow_heart: '
+            },
+            {
+              text: '-1  Unsatisfied :broken_heart: ',
+              value: '-1  Unsatisfied :broken_heart: '
+            },
+            {
+              text: '-2 Very Unsatisfied :broken_heart: :broken_heart:',
+              value: '-2 Very Unsatisfied :broken_heart: :broken_heart:'
+            }
+          ]
+        }
+      ]
+    },
+       {
+      text: ticket.text,
+      fallback: 'Pre-filled because you have actions in your attachment.',
+      color: '#FFE700',
+      text: 'The ability to identify high value targets for individual, team or organizational improvement and proactively prioritize and communicate them.',
+      mrkdwn_in: [
+        'text',
+        'pretext',
+        'fields'
+      ],
+      callback_id: 'Pre-filled because you have actions in your attachment.',
+      attachment_type: 'default',
+      title: 'Work Initiative',
+      actions: [
+        {
+          name: 'Work Initiative',
+          text: 'Work Initiative',
+          type: 'select',
+          value: 'initiative',
+          data_source: 'static',
+          options: [
+            {
+              text: '+2  Very Satisfied :green_heart: :green_heart: ',
+              value: '+2  Very Satisfied :green_heart: :green_heart: '
+            },
+            {
+              text: '+1  Satisfied :green_heart: ',
+              value: '+1  Satisfied :green_heart: '
+            },
+            {
+              text: '0 Neutral :yellow_heart: ',
+              value: '0 Neutral :yellow_heart: '
+            },
+            {
+              text: '-1  Unsatisfied :broken_heart: ',
+              value: '-1  Unsatisfied :broken_heart: '
+            },
+            {
+              text: '-2 Very Unsatisfied :broken_heart: :broken_heart:',
+              value: '-2 Very Unsatisfied :broken_heart: :broken_heart:'
+            }
+          ]
+        }
+      ]
+      
+    },
+       {
+      text: ticket.text,
+      fallback: 'Pre-filled because you have actions in your attachment.',
+      color: '#40BC46',
+      text: 'The ability to understand and make oneself understood via both written and verbal communication. The knowledge to use communications mediums effectively and appropriately. ',
+      mrkdwn_in: [
+        'text',
+        'pretext',
+        'fields'
+      ],
+      callback_id: 'Pre-filled because you have actions in your attachment.',
+      attachment_type: 'default',
+      title: 'Team Communication',
+      actions: [
+        {
+          name: 'Team Communication',
+          text: 'Team Communication',
+          type: 'select',
+          value: 'Communication',
+          data_source: 'static',
+          options: [
+            {
+              text: '+2  Very Satisfied :green_heart: :green_heart: ',
+              value: '+2  Very Satisfied :green_heart: :green_heart: '
+            },
+            {
+              text: '+1  Satisfied :green_heart: ',
+              value: '+1  Satisfied :green_heart: '
+            },
+            {
+              text: '0 Neutral :yellow_heart: ',
+              value: '0 Neutral :yellow_heart: '
+            },
+            {
+              text: '-1  Unsatisfied :broken_heart: ',
+              value: '-1  Unsatisfied :broken_heart: '
+            },
+            {
+              text: '-2 Very Unsatisfied :broken_heart: :broken_heart:',
+              value: '-2 Very Unsatisfied :broken_heart: :broken_heart:'
+            }
+          ]
+        }
+      ]
+    },
+      {
+      text: ticket.text,
+      fallback: 'Pre-filled because you have actions in your attachment.',
+      color: '#297AD4',
+      text: 'The ability to act, react and communicate in a manner that demonstrates respect for coworkers, an understanding of the team\'s goals and acceptance of the organization\'s mission.',
+      mrkdwn_in: [
+        'text',
+        'pretext',
+        'fields'
+      ],
+      callback_id: 'Pre-filled because you have actions in your attachment.',
+      attachment_type: 'default',
+      title: 'Team Professionalism',
+      actions: [
+        {
+          name: 'Team Professionalism',
+          text: 'Team Professionalism',
+          type: 'select',
+          value: 'professionalism',
+          data_source: 'static',
+          options: [
+            {
+              text: '+2  Very Satisfied :green_heart: :green_heart: ',
+              value: '+2  Very Satisfied :green_heart: :green_heart: '
+            },
+            {
+              text: '+1  Satisfied :green_heart: ',
+              value: '+1  Satisfied :green_heart: '
+            },
+            {
+              text: '0 Neutral :yellow_heart: ',
+              value: '0 Neutral :yellow_heart: '
+            },
+            {
+              text: '-1  Unsatisfied :broken_heart: ',
+              value: '-1  Unsatisfied :broken_heart: '
+            },
+            {
+              text: '-2 Very Unsatisfied :broken_heart: :broken_heart:',
+              value: '-2 Very Unsatisfied :broken_heart: :broken_heart:'
+            }
+          ]
+        }
+      ]
+    },
+       {
+      text: ticket.text,
+      fallback: 'Pre-filled because you have actions in your attachment.',
+      color: '#660FAA',
+      text: 'TThe ability to embed oneself as a compatible, vital member of a team and an organization at large. The ability to absorb and reflect a team\'s values.',
+      mrkdwn_in: [
+        'text',
+        'pretext',
+        'fields'
+      ],
+      callback_id: 'Pre-filled because you have actions in your attachment.',
+      attachment_type: 'default',
+      title: 'Team Integration',
+      actions: [
+        {
+          name: 'Team Integration',
+          text: 'Team Integration',
+          type: 'select',
+          value: 'integration',
+          data_source: 'static',
+          options: [
+            {
+              text: '+2  Very Satisfied :green_heart: :green_heart: ',
+              value: '+2  Very Satisfied :green_heart: :green_heart: '
+            },
+            {
+              text: '+1  Satisfied :green_heart: ',
+              value: '+1  Satisfied :green_heart: '
+            },
+            {
+              text: '0 Neutral :yellow_heart: ',
+              value: '0 Neutral :yellow_heart: '
+            },
+            {
+              text: '-1  Unsatisfied :broken_heart: ',
+              value: '-1  Unsatisfied :broken_heart: '
+            },
+            {
+               text: '-2 Very Unsatisfied :broken_heart: :broken_heart:',
+              value: '-2 Very Unsatisfied :broken_heart: :broken_heart:'
+            }
+          ]
+        }
+      ]
+    },
+    ]),
+  })).then((result) => {
+    debug('sendConfirmation: %o', result.data);
+  }).catch((err) => {
+    debug('sendConfirmation error: %o', err);
+    console.error(err);
+  });
+
+  // post to channel
+axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+    token: process.env.SLACK_ACCESS_TOKEN,
+    channel: '#fellow-feedback',
+    text: `Feedback for ${ticket.fellow} has been added by ${ticket.userEmail}`,
+    attachments: JSON.stringify([
+      {
+        //title: `Feedback for ${ticket.fellow} has been added by ${ticket.userEmail}`,
+        // Get this from the 3rd party helpdesk system
+        text: ticket.text,
+        fields: [
+          {
+          title: 'Fellow name:',
+          value: ticket.fellow,
+          'short': true
+        },
+        {
+          title: 'Partner:',
+          value: ticket.company,
+          'short': true
+        },
+        {
+          title: 'Date submitted',
+          value: Date.now(),
+          'short': true
+        },
+        {
+          title: 'Work Quality',
+          value: ticket.quality,
+          'short': true
+        },
+        {
+          title: 'Work Quanitity',
+          value: ticket.quantity,
+          'short': true
+        },
+        ],
+      },
+    ]),
+  })).then((result) => {
+    debug('sendConfirmation: %o', result.data);
+  }).catch((err) => {
+    debug('sendConfirmation error: %o', err);
+    console.error(err); 
+});
+ 
+};
+// Create helpdesk ticket. Call users.find to get the user's email address
+// from their user ID
+const create = (userId, submission) => {
+  const ticket = {};
+
+  const fetchUserEmail = new Promise((resolve, reject) => {
+    users.find(userId).then((result) => {
+      debug(`Find user: ${userId}`);
+      resolve(result.data.user.profile.email);
+    }).catch((err) => { reject(err); });
+  });
+
+  fetchUserEmail.then((result) => {
+    ticket.userId = userId;
+    ticket.userEmail = result;
+    ticket.fellow = submission.fellow;
+    ticket.email = submission.email;
+    ticket.company = submission.company;
+    ticket.quantity = submission.quantity;
+    ticket.quality = submission.quality;
+    sendConfirmation(ticket);
+
+    return ticket;
+  }).catch((err) => { console.error(err); });
+};
+
+module.exports = { create, sendConfirmation };
